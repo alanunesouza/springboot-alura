cc7e4ecedb4c09cf617fce17a21f051cc337152e
Alan Souza <alan.souza@b2wdigital.com>
2020-04-20 01:18:30 -0300

    add authentication configs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$HRnLM.H651PfcFD/BUSHxOUmqHLWs0Aj1F5xT9ilH/mq9f8hdj7FK');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
